10a2b8574b1c33153564b0f1a77db3af566a4ba0
tyson lind <tysonfloydlind@gmail.com>
2023-03-16 13:14:18 -0600

    wrote a function that takes a square matrix array and returns the absolute difference between the two sums of its diagonals

---
+++ diagonalDifference.js
@@ -0,0 +1,13 @@
+let x = ["banana", 1,5,7,null,true];
+
+let z = x.slice();
+console.log(z);
+z.shift();
+console.log(z);
+z.pop();
+console.log(z);
+console.log(z.indexOf(5));
+
+const y = {};
+
+y = "banana";
